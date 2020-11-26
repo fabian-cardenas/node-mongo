021b84062210d09044fa1797cd1c9f1797796187
Fabian <fabian.ecm96@gmail.com>
2020-11-26 01:32:03 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,24 @@
 // Puerto
 //==============
 process.env.PORT = process.env.PORT || 3000;
+
+// ======================
+// ENTORNO
+// ======================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//========================
+// BASE DE DATOS
+// =======================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://jogger:OtEZVIjqwI6ulvAH@cluster0.v4ey7.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config');
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -9,36 +11,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
+
+
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }, (err, res) => {
+
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
 });
-
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-
-});
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
-});
-
 app.listen(process.env.PORT, () => {
     console.log('Esuchando puerto', process.env.PORT);
-})
+});
